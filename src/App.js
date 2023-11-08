25eae8a5fd9dab61566889e109581081b8e423ed
Marisa Wyatt <risawyatt22@gmail.com>
2023-11-08 12:23:27 -0700

    Add first api call

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useState, useEffect } from "react";
+import { getStreams } from "./apicalls";
 
 function App() {
+  const [streams, setStreams] = useState(null);
+
+  useEffect(() => {
+    const fetchStreams = async () => {
+      const streamData = await getStreams();
+      setStreams(streamData);
+    };
+    fetchStreams();
+  }, []);
+
+// move fetch out of use effect and into form component
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1 className="header-name">STREAM SEEKER</h1>
+        {streams && console.log("streams", streams)}
+        {/* HEADER comp: text as <Link>, <navLink> to saved path */}
       </header>
+      {/* Landing Page route, wrapper comp contains: form comp */}
     </div>
   );
 }
