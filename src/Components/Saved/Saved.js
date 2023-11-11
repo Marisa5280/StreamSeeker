1f8e72dcecf05ef40287e74aa0bc740ed3222c59
Marisa Wyatt <risawyatt22@gmail.com>
2023-11-11 10:25:21 -0700

    feat: Write saved component to render saved links

--- src/Components/Saved/Saved.js
+++ src/Components/Saved/Saved.js
@@ -1,7 +1,31 @@
-// import "./Card.css";
+import "./Saved.css";
 
 const Saved = () => {
-  return (null);
+  const data = localStorage.getItem(localKey);
+  return (
+    <div className="results-container">
+      <SavedCards data={data} />
+    </div>
+  );
+};
+
+const SavedCards = ({ data }) => {
+  return !data ? (
+    <p>No saved stream links! Add some!</p>
+  ) : (
+    data.map((propsObj) => {
+      const { platform, id, title, artistName, thumbnailUrl, link } = propsObj;
+      <Card
+        key={platform}
+        platform={platform}
+        title={title}
+        artistName={artistName}
+        thumbnailUrl={thumbnailUrl}
+        id={id}
+        link={link}
+      />;
+    })
+  );
 };
 
 export default Saved;
